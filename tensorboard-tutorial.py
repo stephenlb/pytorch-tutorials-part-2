af3320660e31b2188963416b80c82e872773a523
Stephen Blum <blum.stephen@gmail.com>
2026-04-21 15:17:28 -0700

    full working *maybe* CNN.

--- tensorboard-tutorial.py
+++ tensorboard-tutorial.py
@@ -48,24 +48,25 @@ validation_loader = torch.utils.data.DataLoader(validation_set,
 class Net(torch.nn.Module):
     def __init__(self, classes=10):
         super(Net, self).__init__()
-        self.c1   = torch.nn.Conv2d(1, 6, 10)
-        self.pool = torch.nn.MaxPool2d(2, 2)
-        self.c2   = torch.nn.Conv2d(10, 20, 6)
-        self.l1   = torch.nn.Linear(20 * 3 * 3, 120)
+        self.pool = torch.nn.MaxPool2d(4, 4)
+        self.c1   = torch.nn.Conv2d(3, 6, 10)
+        self.c2   = torch.nn.Conv2d(3, 20, 6)
+        self.c3   = torch.nn.Conv2d(3, 20, 6)
+        self.c4   = torch.nn.Conv2d(3, 20, 6)
+        self.l1   = torch.nn.Linear(20 * 48 * 48, 120)
         self.l2   = torch.nn.Linear(120, 120)
         self.l3   = torch.nn.Linear(120, classes)
 
     def forward(self, inputs):
-        return inputs
-        out = self.pool(F.relu(self.c1(inputs)))
-        return out
-        out = self.pool(F.relu(self.c2(inputs)))
-        out = out.view(-1, 20 * 3 * 3) ## Flatten the 2d matrix to 1d vectors
-        out = F.relu(self.l1(out))
-        out = F.relu(self.l2(out))
+        out = F.gelu(self.c1(inputs))
+        out = self.pool(F.gelu(self.c2(inputs)))
+        out = F.gelu(self.c3(inputs))
+        out = self.pool(F.gelu(self.c4(inputs)))
+        out = out.view(-1, 20 * 48 * 48) ## Flatten the 2d matrix to 1d vectors
+        out = F.gelu(self.l1(out))
+        out = F.gelu(self.l2(out))
         out = self.l3(out)
         return inputs
-
 
 # Helper function for inline image display
 # Extract a batch of 4 images
@@ -75,10 +76,11 @@ writer = SummaryWriter('runs/exp1')
 classes = 36
 model = Net(classes)
 
-for i in range(30):
+for i in range(1):
     image, label = next(dataiter)
     out = model(image)
     print(out)
+    print(out.shape)
     img_grid = torchvision.utils.make_grid(image)
     writer.add_image(f'{i}: Cat or Dog: {label}' , img_grid)
 writer.flush()
